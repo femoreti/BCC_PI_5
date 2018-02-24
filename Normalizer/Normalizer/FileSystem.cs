53398cc5beebfb3a05db3c6cf118b3f57d847cf5
Caio Rosa <caiolrosa1912@gmail.com>
2018-02-24 16:24:22 -0300

    Added C# project to normalize Raw Data

---
+++ Normalizer/Normalizer/FileSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normalizer
+{
+    class FileSystem
+    {
+        public static string[] GetAllFilesInFolder(string Path)
+        {
+            if (!Directory.Exists(Path)) { Console.WriteLine("Path does not exist!"); Environment.Exit(-1); }
+            return Directory.GetFiles(Path);
+        }
+
+        public static string GetFileContents(string Path)
+        {
+            if (!File.Exists(Path)) { Console.WriteLine("Path does not exist!"); Environment.Exit(-1); }
+            return File.ReadAllText(Path);
+        }
+    }
+}
---
+++ Normalizer/Normalizer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FilePath = FileSystem.GetAllFilesInFolder(@"../../Raw Data/Wine")[0];
+            List<string> FileLines = GetFileLines(FilePath);
+            ClearEmptyValues(ref FileLines);
+            ClearOutliers(ref FileLines);
+
+            Console.ReadLine();
+        }
+
+        /** Vai dar ruim caso tenha letra em uma linha
+         *  TODO: Limpar outliers e melhorar o metodo para funcionar por coluna
+         */
+        private static List<double> ClearOutliers(ref List<string> FileLines)
+        {
+            List<double> ConvertedList = new List<double>();
+            foreach(string Item in FileLines)
+            {
+                ConvertedList.AddRange(Item.Split(',').Select(item => double.Parse(item)).ToList());
+            }
+
+            return ConvertedList;
+        }
+
+        private static void ClearEmptyValues(ref List<string> FileLines)
+        {
+            for (int i = 0; i < FileLines.Count(); i++)
+                if (FileLines[i].Split(',').Contains("")) { FileLines.RemoveAt(i); }
+        }
+
+        private static List<string> GetFileLines(string FilePath)
+        {
+            string FileContents = FileSystem.GetFileContents(FilePath);
+            List<string> FileLines = FileContents.Split(Environment.NewLine.ToCharArray()).ToList();
+            FileLines.RemoveAll(item => item == "");
+
+            return FileLines;
+        }
+    }
+}
