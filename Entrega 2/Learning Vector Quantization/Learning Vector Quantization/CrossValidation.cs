5dc6e1dccdef7f67335442772ac9190d010260c4
Felipe Moreti <felipe_moreti7@hotmail.com>
2018-03-24 12:38:23 -0300

    criação do projeto

---
+++ Entrega 2/Learning Vector Quantization/Learning Vector Quantization/CrossValidation.cs
@@ -0,0 +1,150 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Normalizer
+{
+    class CrossValidation
+    {
+        public static Dictionary<string, int> binaryConfusionMatrix = new Dictionary<string, int>();
+        public static Dictionary<string, int> multiClassConfusionMatrix = new Dictionary<string, int>();
+
+        /// <summary>
+        /// Calcula o Erro amostral
+        /// </summary>
+        /// <param name="TestSet"></param>
+        /// <param name="predictions"></param>
+        /// <returns></returns>
+        public static float erroAmostral(List<List<string>> TestSet, List<string> predictions)
+        {
+            float erro = 0;
+            for (int i = 0; i < TestSet.Count; i++)
+            {
+                erro += deltaDeKronecker(TestSet[i].Last(), predictions[i]);
+            }
+
+            return erro / (float)TestSet.Count;
+        }
+
+        private static int deltaDeKronecker(string a, string b)
+        {
+            return (a == b) ? 0 : 1;
+        }
+
+        public static float erroDeValidacaoCruzada(List<float> listOfErroAmostral)
+        {
+            // Calcula a validacao cruzada
+            float crossValidation = 0;
+            foreach (float f in listOfErroAmostral)
+                crossValidation += f;
+
+            crossValidation /= listOfErroAmostral.Count;
+            Console.WriteLine("erro de CROSS VALIDATION: " + (crossValidation * 100).ToString() + "%");
+
+            return crossValidation;
+        }
+
+        public static void prepareConfusionMatrix(List<List<string>> Dataset, List<List<string>> TestSet, List<string> Predictions)
+        {
+            List<string> DistinctClasses = new List<string>();
+            foreach (var Item in Dataset)
+            {
+                DistinctClasses.Add(Item.Last());
+            }
+
+            DistinctClasses = DistinctClasses.Distinct().ToList();
+
+            if(DistinctClasses.Count > 2) // Multiclasses
+            {
+                for (int i = 0; i < TestSet.Count; i++)
+                {
+                    string testClas = TestSet[i].Last();
+
+                    if (!multiClassConfusionMatrix.ContainsKey(testClas + "-" + Predictions[i]))
+                        multiClassConfusionMatrix.Add(testClas + "-" + Predictions[i], 1);
+                    else
+                        multiClassConfusionMatrix[testClas + "-" + Predictions[i]]++;
+                }
+            }
+            else //Binario
+            {
+                for (int i = 0; i < TestSet.Count; i++)
+                {
+                    string testClass = TestSet[i].Last();
+
+                    if(testClass == "1" && Predictions[i] == "1")
+                    {
+                        //TP
+                        if (binaryConfusionMatrix.ContainsKey("TP"))
+                            binaryConfusionMatrix["TP"]++;
+                        else
+                            binaryConfusionMatrix.Add("TP", 1);
+                    }
+                    else if(testClass == "0" && Predictions[i] == "1")
+                    {
+                        //FP
+                        if (binaryConfusionMatrix.ContainsKey("FP"))
+                            binaryConfusionMatrix["FP"]++;
+                        else
+                            binaryConfusionMatrix.Add("FP", 1);
+                    }
+                    else if (testClass == "1" && Predictions[i] == "0")
+                    {
+                        //FN
+                        if (binaryConfusionMatrix.ContainsKey("FN"))
+                            binaryConfusionMatrix["FN"]++;
+                        else
+                            binaryConfusionMatrix.Add("FN", 1);
+                    }
+                    else if (testClass == "0" && Predictions[i] == "0")
+                    {
+                        //TN
+                        if (binaryConfusionMatrix.ContainsKey("TN"))
+                            binaryConfusionMatrix["TN"]++;
+                        else
+                            binaryConfusionMatrix.Add("TN", 1);
+                    }
+                }
+            }
+        }
+
+        public static string GeraMatrizBinaria()
+        {
+            float TP = 0, FP = 0, FN = 0, TN = 0;
+            TP = (binaryConfusionMatrix.ContainsKey("TP")) ? binaryConfusionMatrix["TP"] : 0;
+            FP = (binaryConfusionMatrix.ContainsKey("FP")) ? binaryConfusionMatrix["FP"] : 0;
+            FN = (binaryConfusionMatrix.ContainsKey("FN")) ? binaryConfusionMatrix["FN"] : 0;
+            TN = (binaryConfusionMatrix.ContainsKey("TN")) ? binaryConfusionMatrix["TN"] : 0;
+
+            string confusionOUT = "True Positive\t" + TP.ToString() +
+                    "\nFalse Positive\t" + FP.ToString() +
+                    "\nFalse Negative\t" + FN.ToString() +
+                    "\nTrue Negative\t" + TN.ToString() +
+                    "\nSensibilidade\t" + (TP/(TP+FN)).ToString() + 
+                    "\nEspecificidade\t" + (TN/(TN+FP)).ToString() +
+                    "\nPrecisão\t" + (TP/(TP+FP)).ToString() +
+                    "\nRevocação\t" + (TP/(TP+FN)).ToString();
+
+            //Console.WriteLine(confusionOUT);
+            binaryConfusionMatrix.Clear(); //Limpa todos resultados de classe binaria
+
+            return confusionOUT;
+        }
+
+        public static string GeraMatrizMultiClasse()
+        {
+            string confusionOUT = string.Empty;
+            List<string> keys = multiClassConfusionMatrix.Keys.ToList();
+            for (int i = 0; i < keys.Count; i++)
+            {
+                confusionOUT += keys[i] + "\t" + multiClassConfusionMatrix[keys[i]].ToString() + "\n";
+            }
+
+            multiClassConfusionMatrix.Clear();
+
+            return confusionOUT;
+        }
+    }
+}
---
+++ Entrega 2/Learning Vector Quantization/Learning Vector Quantization/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Vector_Quantization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inicio do Projeto LVQ");
+            Console.ReadLine();
+        }
+    }
+}
