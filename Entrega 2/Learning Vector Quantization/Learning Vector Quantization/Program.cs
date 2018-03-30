659e116c754dc20876cb5b5b8e8c160b8224c379
Felipe Moreti <felipe_moreti7@hotmail.com>
2018-03-30 15:56:45 -0300

    fix winner neuron

--- Entrega 2/Learning Vector Quantization/Learning Vector Quantization/Program.cs
+++ Entrega 2/Learning Vector Quantization/Learning Vector Quantization/Program.cs
@@ -42,7 +42,7 @@ namespace Learning_Vector_Quantization
                 }
             }
 
-            _lvq.RunLVQ(Neuronios, Dataset, 10);
+            _lvq.RunLVQ(Neuronios, Dataset, 5, 10);
 
             for (int i = 1; i <= 4; i++) //Executa os 4 tipos de R
             {
